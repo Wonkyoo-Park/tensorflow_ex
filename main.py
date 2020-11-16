d9da7abf8c807369a431b1980f9d24647e102543
Wonkyoo-Park <javawk@daum.net>
2020-11-16 12:54:37 +0900

    make class Feature Extractor

--- main.py
+++ main.py
@@ -6,18 +6,32 @@ from tensorflow.keras.layers import Conv2D, AveragePooling2D
 from tensorflow.keras.layers import Flatten, Dense, ZeroPadding2D
 
 
+class FeatureExtrator(Layer):
+    def __init__(self, filter1, filter2):
+        super(FeatureExtrator, self).__init__()
+
+        self.conv1 = Conv2D(filters=filter1, kernerl_size=5, padding='valid',
+                            strides=1, activation='tanh')
+        self.conv1_pool = AveragePooling2D(pool_size=2, strides=2)
+        self.conv2 = Conv2D(filters=filter2, kernel_size=5, padding='valid',
+                            strides=1, activation='tanh')
+        self.conv2_pool = AveragePooling2D(pool_size=2, strides=2)
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.conv1_pool(x)
+        x = self.conv2(x)
+        x = self.conv2_pool(x)
+
+        return x
+
+
 class LeNet1(Model):
     def __init__(self):
         super(LeNet1, self).__init__()
 
         # feature extractor
-        self.feature_extractor = Sequential()
-        self.feature_extractor.add(Conv2D(filters=4, kernel_size=5, padding='valid', strides=1,
-                                   activation='tanh'))
-        self.feature_extractor.add(AveragePooling2D(pool_size=2, strides=2))
-        self.feature_extractor.add(Conv2D(filters=12, kernel_size=5, padding='valid', strides=1,
-                                   activation='tanh'))
-        self.feature_extractor.add(AveragePooling2D(pool_size=2, strides=2))
+        self.feature_extractor = FeatureExtrator(4, 12)
 
         # classifier
         self.classifier = Sequential()
@@ -36,13 +50,8 @@ class LeNet4(Model):
         super(LeNet4, self).__init__()
 
         # feature extractor
-        self.feature_extractor = Sequential()
-        self.feature_extractor.add(Conv2D(filters=4, kernel_size=5, padding='valid', strides=1,
-                                          activation='tanh'))
-        self.feature_extractor.add(AveragePooling2D(pool_size=2, strides=2))
-        self.feature_extractor.add(Conv2D(filters=16, kernel_size=5, padding='valid', strides=1,
-                                          activation='tanh'))
-        self.feature_extractor.add(AveragePooling2D(pool_size=2, strides=2))
+        self.zero_padding = ZeroPadding2D(padding=2)
+        self.feature_extractor = FeatureExtrator(4, 16)
 
         # classifier
         self.classifier = Sequential()
@@ -63,13 +72,7 @@ class LeNet5(Model):
 
         # feature extractor
         self.zero_padding = ZeroPadding2D(padding=2)
-        self.feature_extractor = Sequential()
-        self.feature_extractor.add(Conv2D(filters=6, kernel_size=5, padding='valid', strides=1,
-                                          activation='tanh'))
-        self.feature_extractor.add(AveragePooling2D(pool_size=2, strides=2))
-        self.feature_extractor.add(Conv2D(filters=16, kernel_size=5, padding='valid', strides=1,
-                                          activation='tanh'))
-        self.feature_extractor.add(AveragePooling2D(pool_size=2, strides=2))
+        self.feature_extractor = FeatureExtrator(6, 16)
 
         # classifier
         self.classifier = Sequential()
@@ -83,4 +86,3 @@ class LeNet5(Model):
         x = self.classifier(x)
 
         return x
-
