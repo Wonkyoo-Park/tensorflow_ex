166b5297e20ecfcf829b532cd24b33b1a4ab7118
Wonkyoo-Park <javawk@daum.net>
2020-11-16 11:14:36 +0900

    Object Oriented

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
@@ -51,3 +52,103 @@ LeNet5.build(input_shape=(None, 28, 28, 1))
 LeNet5.summary()
 
 
+=======
+import tensorflow as tf
+
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import Conv2D, AveragePooling2D
+from tensorflow.keras.layers import Flatten, Dense, ZeroPadding2D
+
+
+class LeNet1(Model):
+    def __init__(self):
+        super(LeNet1, self).__init__()
+
+        # feature extractor
+        self.conv1 = Conv2D(filters=4, kernel_size=5, padding='valid', strides=1,
+                            activation='tanh')
+        self.conv1_pool = AveragePooling2D(pool_size=2, strides=2)
+        self.conv2 = Conv2D(filters=12, kernel_size=5, padding='valid', strides=1,
+                            activation='tanh')
+        self.conv2_pool = AveragePooling2D(pool_size=2, strides=2)
+
+        # claassifier
+        self.flatten = Flatten()
+        self.dense1 = Dense(units=10, activation='softmax')
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.conv1_pool(x)
+        x = self.conv2(x)
+        x = self.conv2_pool(x)
+        x = self.flatten(x)
+        x = self.dense1(x)
+
+        return x
+
+
+class LeNet4(Model):
+    def __init__(self):
+        super(LeNet4, self).__init__()
+
+        # feature extractor
+        self.zero_padding = ZeroPadding2D(padding=2)
+        self.conv1 = Conv2D(filters=4, kernel_size=5, padding='valid', strides=1,
+                            activation='tanh')
+        self.conv1_pool = AveragePooling2D(pool_size=2, strides=2)
+        self.conv2 = Conv2D(filters=16, kernel_size=5, padding='valid', strides=1,
+                            activation='tanh')
+        self.conv2_pool = AveragePooling2D(pool_size=2, strides=2)
+
+        # claassifier
+        self.flatten = Flatten()
+        self.dense1 = Dense(units=120, activation='tanh')
+        self.dense2 = Dense(units=10, activation='softmax')
+
+    def call(self, x):
+        x = self.zero_padding(x)
+        x = self.conv1(x)
+        x = self.conv1_pool(x)
+        x = self.conv2(x)
+        x = self.conv2_pool(x)
+        x = self.flatten(x)
+        x = self.dense1(x)
+        x = self.dense2(x)
+
+        return x
+
+
+class LeNet5(Model):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+
+        # feature extractor
+        self.zero_padding = ZeroPadding2D(padding=2)
+        self.conv1 = Conv2D(filters=6, kernel_size=5, padding='valid', strides=1,
+                            activation='tanh')
+        self.conv1_pool = AveragePooling2D(pool_size=2, strides=2)
+        self.conv2 = Conv2D(filters=16, kernel_size=5, padding='valid', strides=1,
+                            activation='tanh')
+        self.conv2_pool = AveragePooling2D(pool_size=2, strides=2)
+
+        # claassifier
+        self.flatten = Flatten()
+        self.dense1 = Dense(units=140, activation='tanh')
+        self.dense2 = Dense(units=84, activation='tanh')
+        self.dense3 = Dense(units=10, activation='softmax')
+
+    def call(self, x):
+        x = self.zero_padding(x)
+        x = self.conv1(x)
+        x = self.conv1_pool(x)
+        x = self.conv2(x)
+        x = self.conv2_pool(x)
+        x = self.flatten(x)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        x = self.dense3(x)
+
+        return x
+
+>>>>>>> Stashed changes
